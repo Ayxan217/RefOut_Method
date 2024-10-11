2d3b0c8efd734e487b64db69cba90326d658d66c
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-11 10:51:55 +0400

    task added

---
+++ Method_RefTask/Method_RefTask/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Method_RefTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] arr = { 4, 8, 9, 0, 12, 5, 3 };
+
+            Console.Write("elave olunacaq elemrntlerin sayı: ");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(" elemntleri daxil edin: ");
+                int newNumber = int.Parse(Console.ReadLine());
+
+                CustomResize(ref arr, newNumber);
+            }
+            Console.WriteLine("yeni array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        public static void CustomResize(ref int[] arr, int newElement)
+        {
+
+            int[] newArr = new int[arr.Length + 1];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+
+            newArr[newArr.Length - 1] = newElement;
+            arr = newArr;
+        }
+    }
+       
+    
+}
+
